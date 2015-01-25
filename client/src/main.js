92c2e709d2f06d4d926a3a24cd947e119a9fdafc
Bruce Coddington <brucecoddington@gmail.com>
2015-01-24 22:32:18 -0600

    set up build for semantic to prepare for convert app from bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,6 @@
 /** @jsx React.DOM */
 window.$ = require('jquery');
-require('semantic-ui/dist/semantic');
+require('../../semantic-ui/dist/semantic');
 
 var React = window.React = require('react');
 var Router = require('react-router');
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var
   filesize =    require('gulp-filesize'),
   jade =        require('gulp-jade'),
   jest =        require('gulp-jest'),
+  jshint =      require('gulp-jshint'),
   less =        require('gulp-less'),
   livereload =  require('gulp-livereload'),
   cssmin =      require('gulp-minify-css'),
@@ -32,28 +33,31 @@ gulp.task('dev',  ['core', 'jade:dev']);
 gulp.task('prod', ['core', 'jade:prod', 'uglify']);
 
 // server tasks
-gulp.task('serve:dev',  shell.task([pkg.scripts.run_dev]));
-gulp.task('serve:prod', shell.task([pkg.scripts.run_prod]));
-gulp.task('debug',      shell.task([pkg.scripts.debug]));
+gulp.task('serve:dev', function () {
+  nodemon({
+    script: './api/server.js',
+    env: {'NODE_ENV': 'development'}
+  });
+});
+
+gulp.task('serve:prod', function () {
+  nodemon({
+    script: './api/server.js',
+    env: {'NODE_ENV': 'production'}
+  });
+});
+
+gulp.task('debug', shell.task([pkg.scripts.debug]));
 
 // setup the global watches
 gulp.task('watch:dev', function () {
-  gulp.watch(client('/less/**/*.less'), function () {
-    gulp.start('concat:css');
-  });
-
+  gulp.watch([client('/less/**/*.less'), './semantic-ui/src/**/*'], ['concat:css']);
   gulp.start('dev');
 });
 
 gulp.task('watch:prod', function () {
-  watch(client('/less/**/*.less'), function () {
-    gulp.start('concat:css');
-  });
-
-  watch(dist('/js/app.js'), function () {
-    gulp.start('uglify');
-  });
-
+  gulp.watch([client('/less/**/*.less'), './semantic-ui/src/**/*'], ['concat:css']);
+  gulp.watch([dist('/js/app.js')], ['uglify']);
   gulp.start('prod');
 });
 
@@ -116,43 +120,33 @@ gulp.task('copy:assets', ['clean:assets'], function () {
   return merge(fa, img);
 });
 
-gulp.task('copy:semantic', function (cb) {
-  gulp.src('./node_modules/semantic-ui/src/**')
+// semantic ui tasks
+gulp.task('semantic:copy', function (cb) {
+  var src = gulp.src('./node_modules/semantic-ui/src/**')
     .pipe(gulp.dest('./semantic-ui/src'));
 
-  gulp.src('./node_modules/semantic-ui/tasks/**')
+  var tasks = gulp.src('./node_modules/semantic-ui/tasks/**')
     .pipe(gulp.dest('./semantic-ui/tasks'));
 
-  gulp.src([
+  var files = gulp.src([
     './node_modules/semantic-ui/gulpfile.js',
     './node_modules/semantic-ui/package.json',
     './node_modules/semantic-ui/semantic.json.example'
   ])
     .pipe(gulp.dest('./semantic-ui'));
+
+  return merge(src, tasks, files);
 });
 
-gulp.task('install:semantic', shell.task(['cd semantic-ui && npm install && gulp install']));
-gulp.task('build:semantic',   shell.task(['cd semantic-ui && gulp build']));
+gulp.task('semantic:install', ['semantic:copy'], shell.task(['cd semantic-ui && npm install && gulp install']));
+gulp.task('semantic:build',   shell.task(['cd semantic-ui && gulp build']));
 
 // Compile and concatenate less into css
-gulp.task('clean:css', function (cb) {
-  return del([dist('/css')], cb);
-});
-
-gulp.task('less', ['clean:css'], function () {
-  return gulp.src(client('/less/style.less'))
-    .pipe(plumber())
-    .pipe(less({paths: [client('/less')]}))
-    .pipe(gulp.dest(dist('/css')))
-    .on('error', gutil.log.bind(gutil, 'Error compiling Less'));
-});
-
-gulp.task('concat:css', ['less'], function () {
+gulp.task('concat:css', ['semantic:build'], function () {
   return gulp.src([
       './node_modules/select2/select2.css',
       './node_modules/nprogress/nprogress.css',
-      './node_modules/semantic-ui/dist/semantic.css',
-      dist('/css/style.css')
+      './semantic-ui/dist/semantic.css'
     ])
     .pipe(concat('style.css'))
     .pipe(gulp.dest(dist('/css')))
