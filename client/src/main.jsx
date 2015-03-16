bde2356dd27d4b0a1cdf8227eafee89142f43f01
Bruce Coddington <brucecoddington@gmail.com>
2015-03-15 19:32:07 -0500

    super power hello world

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,30 +1,4 @@
 var React = require('react/addons');
-
-var Hello = React.createClass({
-
-  getInitialState: function () {
-    return {
-      greeting: 'Howdy!!'
-    };
-  },
-
-  getDefaultProps: function () {
-    return {
-      friend: 'Partner!!'
-    };
-  },
-
-  render : function () {
-
-   return (
-    <div className="ui message">
-      <div className="ui huge header">{this.state.greeting}</div>
-      <div className="ui large header">{this.props.friend}</div>
-      <p>You are now an expert Browserifier</p>
-    </div>
-   );
- }
-});
-
+var Hello = require('./hello');
 
 React.render(<Hello />, document.getElementById('app'));
