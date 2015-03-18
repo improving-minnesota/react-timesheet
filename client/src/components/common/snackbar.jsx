0497e9311782b42389a02832692fd46e56d52ba1
Bruce Coddington <brucecoddington@gmail.com>
2015-03-17 20:49:37 -0500

    adding the requires back

--- client/src/components/common/snackbar.jsx
+++ client/src/components/common/snackbar.jsx
@@ -1,5 +1,10 @@
 var React = require('react/addons');
 
+var classes = require('react-classes');
+
+var SnackbarStore = require('../../stores/snackbar.store');
+var SnackbarActions = require('../../actions/snackbar.actions');
+
 var Snackbar = React.createClass({
 
   // TODO - implement me!!
