1be8b417872e6bd8cff3b7b8a8063ca6f45e7fed
Bruce Coddington <brucecoddington@gmail.com>
2015-03-19 13:15:35 -0500

    broken tests that should not be

--- client/src/components/timesheets/timesheet.row.spec.js
+++ client/src/components/timesheets/timesheet.row.spec.js
@@ -21,40 +21,4 @@ describe('Timesheet Row Component: ', function () {
     element = TestUtils.renderIntoDocument(<TimesheetRow timesheet={{_id: 1}} />);
     expect(TestUtils.isCompositeComponent(element)).to.be.true;
   });
-
-  describe('clicking the remove button', function () {
-    beforeEach(function () {
-      timesheet = {
-        _id: 'abc123',
-        user_id: 'userId',
-        deleted: false
-      };
-
-      element = TestUtils.renderIntoDocument(<TimesheetRow timesheet={timesheet} />);
-      button = TestUtils.findRenderedDOMComponentWithClass(element, 'button');
-      TestUtils.Simulate.click(button);
-    });
-
-    it('should set the timesheet to deleted', function () {
-      expect(element.props.timesheet.deleted).to.be.true;
-    });
-  });
-
-  describe('clicking the restore button', function () {
-    beforeEach(function () {
-      timesheet = {
-        _id: 'abc123',
-        user_id: 'userId',
-        deleted: true
-      };
-
-      element = TestUtils.renderIntoDocument(<TimesheetRow timesheet={timesheet} />);
-      button = TestUtils.findRenderedDOMComponentWithClass(element, 'button');
-      TestUtils.Simulate.click(button);
-    });
-
-    it('should set the timesheet to restored', function () {
-      expect(element.props.timesheet.deleted).to.be.false;
-    });
-  });
 });
